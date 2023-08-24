ae3e570e790f8f3597e1796d949b0efc074a17b6
Rohit Tokala <rtokala@purdue.edu>
2023-08-24 07:51:49 -0400

    Configure Drizzle

---
+++ app/lib/client.ts
@@ -0,0 +1,23 @@
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client/http";
+import * as schema from "../../drizzle/schema";
+import type { AppLoadContext } from "@remix-run/cloudflare";
+
+interface Env {
+  TURSO_DB_AUTH_TOKEN?: string;
+  TURSO_DB_URL?: string;
+}
+
+export function buildDbClient(context: AppLoadContext) {
+  const url = (context.env as unknown as Env).TURSO_DB_URL?.trim();
+  if (url === undefined) {
+    throw new Error("TURSO_DB_URL is not defined");
+  }
+
+  const authToken = (context.env as unknown as Env).TURSO_DB_AUTH_TOKEN?.trim();
+  if (authToken === undefined) {
+    throw new Error("TURSO_DB_AUTH_TOKEN is not defined");
+  }
+
+  return drizzle(createClient({ url, authToken }), { schema });
+}
